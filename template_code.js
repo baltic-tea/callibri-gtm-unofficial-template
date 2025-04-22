9fc5eb3ec5a5ec451706bb4d5246d6f72e1576b7
baltic-tea <yes.tea@protonmail.com>
2025-04-22 20:10:27 +0300

    Update template_code.js

--- template_code.js
+++ template_code.js
@@ -3,32 +3,39 @@ const queryPermission = require('queryPermission');
 const copyFromWindow = require('copyFromWindow');
 const log = require('logToConsole');
 
-// Loading Callibri script 
+// Load script 
 function loadScript(url, onFailure) {
-    if (queryPermission('inject_script', url)) {
-        onFailure = onFailure || data.gtmOnFailure;
-        injectScript(
-            url,
-            () => {
-                log("⬇️ Callibri: Loaded script from " + url);
-                if (typeof copyFromWindow("callibriInit") === 'function') {
-                    log("✅ Callibri: Script initialized successfully");
-                    data.gtmOnSuccess();
-                } else {
-                    onFailure();
-                }
-            },
-            () => {
-                log("❌ Callibri: Failed to load Callibri script from " + url);
-                onFailure();
-            },
-            url
-        );
-    }
+  if (queryPermission('inject_script', url)) {
+    onFailure = onFailure || data.gtmOnFailure;
+    injectScript(
+      url,
+      () => {
+        log("⬇️ Callibri: Loaded script from " + url);
+        if (typeof copyFromWindow("callibriInit") === 'function') {
+          log("✅ Callibri: Script initialized successfully");
+          data.gtmOnSuccess();
+        } else {
+          onFailure();
+        }
+      },
+      () => {
+        log("❌ Callibri: Failed to load Callibri script from " + url);
+        onFailure();
+      },
+      url
+    );
+  }
 }
 
-// Trying to load the script from different CDNs
+// Retry to load the script from a different URL
+function retryLoadScript() {
+  var url = "https://cdn.callibri.ru/callibri.js";
+  log("⚠️ Callibri: Script will be loaded from " + url);
+  log("⚠️ When integrating Callibri via GTM, it is recommended to load the script from gcdn.callibri.ru/callibri.js");
+  loadScript(url);
+}
+
 loadScript(
-    "https://gcdn.callibri.ru/callibri.js",
-    () => { loadScript("https://cdn.callibri.ru/callibri.js"); }
+  "https://gcdn.callibri.ru/callibri.js",
+  retryLoadScript
 );
